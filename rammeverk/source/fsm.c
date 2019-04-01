cc9b9fffddde1a4b82f3b3846d176f9fcc4efd53
jaf111 <jan_aleksander@hotmail.com>
2019-04-01 17:12:18 +0200

    added fsm_doors_open, fixed idle

--- rammeverk/source/fsm.c
+++ rammeverk/source/fsm.c
@@ -3,7 +3,7 @@
 
 #include <assert.h>
 #include <stdlib.h>
-
+int last_floor;
 
 static State state;
 
@@ -12,9 +12,8 @@ void FSM_init(){
     if(elev_get_floor_sensor_signal != -1){
         FSM_idle();
     }else{
-        while(elev_get_floor_sensor_signal == -1){
-            elev_set_motor_direction(DIRN_DOWN);
-        }
+        elev_set_motor_direction(DIRN_DOWN);
+        while(elev_get_floor_sensor_signal == -1){}
         FSM_idle();
         return 1;
     }
@@ -35,10 +34,31 @@ State FSM_get_state(){
 void FSM_idle(){
     elev_set_motor_direction(0);
     while(!check_queue()){}
-    if (get_direction() ==0){
+    //sjekker om det kommer en ordre i samme etasje eller en annen
+    if (check_queue_floor(elev_get_floor_sensor_signal())){
         FSM_set_state(DOORS_OPEN);
     }else{
-        elev_set_motor_direction(get_direction());
         FSM_set_state(DRIVE);
     }
 }
+
+void FSM_drive(){
+    last_floor = elev_get_floor_sensor_signal();
+    elev_set_motor_direction(get_direction());
+    while(elev_get_floor_sensor_signal == -1){}
+    if(check_queue_floor(elev_get_floor_sensor_signal())){
+        elev_set_motor_direction(0);
+        FSM_set_state(DOORS_OPEN);
+    }
+}
+
+void FSM_doors_open(){
+    if(elev_get_floor_sensor_signal == -1){
+        printf("heisen er ikke i en etasje");
+    }
+    elev_set_floor_indicator(elev_get_floor_sensor_signal());
+}
+
+//gjør ferdig doorsopen
+//legg til lys
+//legg til doxygen
