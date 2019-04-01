c63b5c2171aa1f47d3c547a1dd68710a2ac1a201
mariusud <mariusudnaes@gmail.com>
2019-04-01 17:05:38 +0200

    added delete_orders, started set_dir implement

--- rammeverk/source/queue.c
+++ rammeverk/source/queue.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 
 
-int arr_opp[N_FLOORS]; // mulig det må brukes static int
-int arr_ned[N_FLOORS]; // ned og opp er for eksterne heistilkallinger
-int arr_destination[N_FLOORS];
+static int arr_opp[N_FLOORS-1]; // mulig det må brukes static int
+static int arr_ned[N_FLOORS-1]; // ned og opp er for eksterne heistilkallinger
+static int arr_destination[N_FLOORS-1];
+int floor;
 
 // opp = 1 betyr opp, ned = 1 betyr ned
 
@@ -14,20 +15,20 @@ int arr_destination[N_FLOORS];
 int check_queue(){
     int i;
     for (i=0; i < N_FLOORS; i++){
-        if (arr_opp[i] == 1) //set lamp
+        if(elev_get_button_signal(BUTTON_CALL_UP,i))
+            arr_opp[i] = 1;
             return 1;
-        
-        if (arr_ned[i] == 1)
-            //set lamp
+        if(elev_get_button_signal(BUTTON_CALL_DOWN,i))
+            arr_ned[i] = 1;
             return 1;
-        
-        if (arr_destination[i] == 1)
-            //set lamp
+        if(elev_get_button_signal(BUTTON_COMMAND,i))
+            arr_destination[i] = 1;
             return 1;
-        
         else
+        {
             return 0;
-    }
+        }
+        
 }
 
 
@@ -41,14 +42,42 @@ int check_queue_floor(int floor){
     if (arr_destination[floor]){
         return 1;
     }
+    else{
+        return 0;
+    }
 }
 
 
 void delete_floor_order(int floor){
-    
+    // delete lamps aswell
+    arr_opp[floor] = 0;
+    arr_ned[floor] = 0;
+    arr_destination[floor] = 0;
+
+}
+
+void delete_all_orders(){
+    int i;
+    for (i = 0; i < N_FLOORS;i++){
+        delete_floor_order(i);
+    }
 }
 
 
 void Add_order(int button){
+
+}
+
+
+
+
+void set_direction(){
+    if (check_queue){
+        if (elev_get_floor_sensor_signal == floor)
+            return 0; //do something
+    }else{
+        elev_get_button_signal(BUTTON_CALL_UP,floor);
+        //set direction 
+    }
 
 }
--- rammeverk/source/queue.h
+++ rammeverk/source/queue.h
@@ -1,3 +1,10 @@
+
+
+
+/**
+  Check for order on all floors and in elevator
+  returns 1 if there exists an order, 0 else
+*/
 void Add_order(int button);
 
 
@@ -11,9 +18,16 @@ int check_queue();
 
 /**
   Check for order on input floor 
-  returns 1 if there exists an order, 0 else
+  returns 1 if there exists an order in the floor, 0 else
 */
 int check_queue_floor(int floor);
+
+
+
+/**
+  
+*/
+int should_stop(int floor);
 
 
 /**
@@ -23,7 +37,7 @@ void delete_floor_order(int floor);
 
 
 /**
-  deletes arr_opp, arr_ned, arr_destination
+  deletes arr_opp, arr_ned, arr_destination on all floors using delete_floor_order
 */
 void delete_all_orders();
 
@@ -33,3 +47,8 @@ void delete_all_orders();
   returns true if there exists an order
 */
 void set_direction();
+
+int should_stop();
+
+int get_direction();
+
