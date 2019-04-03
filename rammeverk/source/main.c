554a9e09e97dea013c356b6f39dd907001a6207d
marius <adrianghnsh@gmail.com>
2019-04-03 16:27:12 +0200

    added timer to makefile and main

--- rammeverk/source/main.c
+++ rammeverk/source/main.c
@@ -1,5 +1,6 @@
 #include "elev.h"
 #include "fsm.h"
+#include "timer.h"
 #include "queue.h"
 #include <time.h>
 #include <stdio.h>
@@ -7,6 +8,7 @@
 
 int main() {
     // Initialize hardware
+
     if (!elev_init()) {
         printf("Unable to initialize elevator hardware!\n");
         return 1;
@@ -32,6 +34,7 @@ int main() {
             case IDLE:
                 //printf("idle");
                 //checks if there are any orders
+                printf("check floor: %d \n ",(check_queue_floor(current_floor)));
                 if (check_queue()){
                     printf("queue");
                     //checks if order is in current floor
@@ -59,7 +62,6 @@ int main() {
                printf("check floor: %d\n", check_queue_floor(current_floor));
 */              printf("check floor: %d \n ",(check_queue_floor(current_floor)));
                 if (check_queue_floor(current_floor)){
-                    //stop elevator
                     printf("Kommer inn til drive switch");
                     elev_set_motor_direction(DIRN_STOP);
                     //turns off lights at last floor
@@ -72,26 +74,40 @@ int main() {
             //printf("current floor: %d \n", (current_floor));
             case DOORS_OPEN:
             
-                printf("doors");
+                //printf("doors");
                 if(current_floor == -1){
                     printf("heisen er ikke i en etasje");
                 }
-                //timer
+                if(!is_timer_on()){
+                    //turns on timer if it is not already on
+                    timer_start();
+                }
+                delete_floor_order(current_floor);
+                //turns on doorlamp
+                elev_set_door_open_lamp(1);
+                //checks if timer has exceeded three seconds
+                if(timer_three_seconds()){
+                    //turns off doorlamp and timer
+                    elev_set_door_open_lamp(0);
+                    turn_off_timer();
+                    //checks if there are any more orders
+                    if(check_queue()){
+                        printf("math checks out");
+                        //finds out which direction to drive in
+                        elev_set_motor_direction(get_direction(current_floor));
+                        //current_direction = get_direction(current_floor);
+                        //Drive
+                        state = DRIVE;
+                    }else{
+                        //Idle
+                        state = IDLE;
+                }
+                }
                 //elev_set_door_open_lamp(int value);
                 //timer
 
-                if(check_queue()){
-                    //finds out which direction to drive in
-                    elev_set_motor_direction(get_direction(current_floor));
-                    //current_direction = get_direction(current_floor);
-                    //Drive
-                    printf("Kommer inn hit");
-                    state = DRIVE;
-                }else{
-                    //Idle
-                    state = IDLE;
-                }
-                delete_floor_order(current_floor);
+                
+                
 
             case EMERGENCY_STOP:
                 for(int i = 0; i < 4; i++)
