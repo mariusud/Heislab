047ae4d92708b5155a5293f1187ce135b317a8e3
kidneb7 <bendia@stud.ntnu.no>
2019-04-03 15:59:39 +0200

    fixed documentation for queue

--- rammeverk/source/queue.h
+++ rammeverk/source/queue.h
@@ -1,45 +1,52 @@
 
 extern elev_motor_direction_t direction;
 
+
+/**
+  @brief initilializes queue arrays.
+*/
 void init_arrays();
 /**
- Needs to be running all the time. 
- if a button is pushed, adds the order to the floor 
-returns 1 if there IS an order, 0 else
+ @brief If a button is pushed, adds the order to the floor. Needs to be running all the time. 
+@returns 1 if there IS an order, 0 else
 */
 int check_queue();
 
 /**
-  Check for order on input floor 
-  returns 1 if there exists an order in the floor, 0 else
+  @brief Check for order on input floor.
+  @returns 1 if there exists an order in the floor, 0 else
 */
 int check_queue_floor(int floor);
 
 /**
-  delete all orders on input floor
+  @brief delete all orders on input floor
 */
 void delete_floor_order(int floor);
 
 /**
-  deletes arr_opp, arr_ned, arr_destination on all floors using delete_floor_order
+  @brief deletes arr_opp, arr_ned, arr_destination on all floors. uses delete_floor_order
 */
 void delete_all_orders();
 
 /**
-  checks if there are any orders above the input floor
+  @brief checks if there are any orders above the input floor.
+  @returns 1 if true, 0 else
 */
 int order_above(int floor);
 
 /**
-  checks if there are any orders below the input floor
+  @brief checks if there are any orders below the input floor.
+  @returns 1 if true, 0 else
 */
 int order_below(int floor);
 
 /**
+  @brief tells the elevator which way to go.
   Check for order on all floors and in elevator
   iterate through whole lists to see if orders above, below or in current floor
-  returns 1 if direction up, 0 if order in current floor or -1 if direction down
   floor is the last locally known floor the elevator is in
+  @returns 1 if direction up, 0 if order in current floor or -1 if direction down
+  
 */
 elev_motor_direction_t get_direction(int floor);
 
